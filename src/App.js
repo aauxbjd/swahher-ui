0f36b68769c7a1a9aadd0de8045b30a81cb1109c
ayush <ayush@ayushs-MacBook-Pro.local>
2023-02-06 03:19:43 -0500

    checkbox above operation

--- src/App.js
+++ src/App.js
@@ -18,16 +18,17 @@ function App() {
       const checkbox = document.createElement("input");
       checkbox.type = "checkbox";
       checkbox.style.marginRight = "10px";
-      
-      // checkbox.addEventListener("click", () => {
-      //   const methodCheckboxes = element.parentElement.getElementsByClassName("opblock-summary")[0].getElementsByTagName("input");
-      //   Array.from(methodCheckboxes).forEach((methodCheckbox) => {
-      //     methodCheckbox.checked = checkbox.checked;
-      //   });
-      // })
+
 
       if (!element.firstChild || element.firstChild.nodeName !== "INPUT") {
-        element.insertBefore(checkbox, element.firstChild);
+
+
+        if (element.className === "opblock-tag") {
+          element.insertAdjacentElement("beforebegin", checkbox)
+        }
+        else
+        
+          element.insertBefore(checkbox, element.firstChild);
       }
     });
   };
@@ -37,8 +38,6 @@ function App() {
       <button onClick={handleLoad}>show</button>
       <SwaggerUI
         url={swaggerUrl}
-        // onComplete={handleLoad}
-        // custom-layout={handleLoad}
       />
     </div>
   );
